3977a5336c2b54950e784e4f831800aeac6d0801
Aravin7 <aravinth.ati@gmail.com>
2022-02-20 19:16:18 +0530

    add-blog-controller

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(jwt());
 // api routes
 app.use("/users", require("./users/users.controller"));
 app.use("/items", require("./items/items.controller"));
+app.use("/blogs", require("./blogs/blogs.controller"));
 
 // global error handler
 app.use(errorHandler);
